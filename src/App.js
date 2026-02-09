95435b917abe25eb8451041c263c3b4b0cbf3350
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-02-09 21:36:46 +0600

    Fix HTML entity in Dashboard breadcrumb for proper rendering

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
         <main className="flex-1 p-4 sm:p-6 lg:p-8">
           {/* Threat Detection Cards */}
           <div className="mb-8">
-            <div className="mb-5 text-xs font-medium">Dashboard > <span className="text-[#0069F7] ">Dashboard</span></div>
+            <div className="mb-5 text-xs font-medium">Dashboard &gt; <span className="text-[#0069F7] ">Dashboard</span></div>
             <ThreatCalendar 
               generateCalendarDays={generateCalendarDays}
               getIntensityColor={getIntensityColor}
@@ -65,6 +65,8 @@ function App() {
           {/* Top Threats Table */}
           <ThreatsTable  />
         </main>
+
+        
       </div>
     </div>
   )
