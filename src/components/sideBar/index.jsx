08bce4566dc943dcccfd09df68e5a27949ad2d46
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-02-09 20:26:43 +0600

    Update logo container padding in SideBar component

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -30,7 +30,7 @@ const SideBar = ({ selectedMenu, setSelectedMenu, sidebarOpen, setSidebarOpen })
         <aside className={`fixed lg:static inset-y-0 left-0 z-30 w-64 bg-blue-600 transform transition-transform duration-300 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
             <div className="h-full flex flex-col">
                 {/* Logo */}
-                <div className="p-6 flex">
+                <div className="px-6 pt-6 flex">
                     <div className="w-12 h-12 bg-white rounded-[20px] p-3 flex items-center justify-center">
                         <img src="./logo.svg" alt="Logo" />
                     </div>
