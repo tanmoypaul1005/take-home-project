018fcef24d407061af826d31fce3ee8a0e8fc27e
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-02-09 20:25:23 +0600

    Refactor SideBar component to use logo image and update Users SVG

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -30,11 +30,9 @@ const SideBar = ({ selectedMenu, setSelectedMenu, sidebarOpen, setSidebarOpen })
         <aside className={`fixed lg:static inset-y-0 left-0 z-30 w-64 bg-blue-600 transform transition-transform duration-300 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
             <div className="h-full flex flex-col">
                 {/* Logo */}
-                <div className="p-6 flex items-center justify-center border-b border-blue-500">
-                    <div className="w-12 h-12 bg-white rounded-lg flex items-center justify-center">
-                        <svg className="w-8 h-8 text-blue-600" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6z" />
-                        </svg>
+                <div className="p-6 flex">
+                    <div className="w-12 h-12 bg-white rounded-[20px] p-3 flex items-center justify-center">
+                        <img src="./logo.svg" alt="Logo" />
                     </div>
                 </div>
 
